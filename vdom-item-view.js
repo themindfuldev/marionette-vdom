f06f1629aeedb4eb1a0d7133f6d1bfbb7fb728bd
Tiago Romero Garcia <tiagoromerogarcia@gmail.com>
2014-12-30 17:22:06 -0800

    making the last test to pass!

--- vdom-item-view.js
+++ vdom-item-view.js
@@ -9,24 +9,6 @@ var createElement = require('virtual-dom/create-element');
 var virtualize = require('vdom-virtualize');
 
 module.exports = Marionette.ItemView.extend({
-  // setElement: function(element, delegate) {
-  //   if (this.$el) this.undelegateEvents();
-
-  //   this.el = element instanceof Backbone.$ ? element[0] : Backbone.$(element)[0];
-
-  //   if (this.el) {
-  //     this.virtualEl = virtualize(this.el);
-  //     this.el = createElement(this.virtualEl);
-  //   }
-
-  //   this.$el = Backbone.$(this.el);
-
-  //   if (delegate !== false) this.delegateEvents();
-
-  //   _.invoke(this._behaviors, 'proxyViewProperties', this);
-  //   return this;
-  // },
-
   setElement: function(element, delegate) {
     Marionette.ItemView.prototype.setElement.apply(this, arguments);
     if (this.el) {
@@ -43,8 +25,6 @@ module.exports = Marionette.ItemView.extend({
   attachElContent: function(html) {
     var newVirtualEl = virtualize.fromHTML(html);
     var patches = diff(this.virtualEl, newVirtualEl);
-    // this.el = patch(this.el, patches);
-    // this.$el = Backbone.$(this.el);
     this.rootEl = patch(this.rootEl, patches);
     this.virtualEl = newVirtualEl;
     return this;
