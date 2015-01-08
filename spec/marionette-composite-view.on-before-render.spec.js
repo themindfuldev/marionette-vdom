8a407bfc2db0eae29b0f7d4b169c761b8475e7e2
Tiago Romero Garcia <tiagoromerogarcia@gmail.com>
2015-01-07 20:12:16 -0800

    adding some more tests from marionette

---
+++ spec/marionette-composite-view.on-before-render.spec.js
@@ -0,0 +1,22 @@
+require('./setup/init');
+
+describe('Marionette.CompositeView - on before render', function() {
+  'use strict';
+
+  beforeEach(function() {
+    this.onBeforeRenderStub = this.sinon.stub();
+    this.renderTemplateStub = this.sinon.stub();
+
+    this.CompositeView = VDOMCompositeView.extend({
+      onBeforeRender: this.onBeforeRenderStub,
+      _renderTemplate: this.renderTemplateStub
+    });
+
+    this.compositeView = new this.CompositeView();
+    this.compositeView.render();
+  });
+
+  it('should call onBeforeRender before rendering the model', function() {
+    expect(this.renderTemplateStub).to.have.been.calledAfter(this.onBeforeRenderStub);
+  });
+});
